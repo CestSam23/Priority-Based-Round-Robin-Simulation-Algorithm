d98a25baa4a6f790a6ac610394f82c58e8cbda9f
Sam <samfertap@gmail.com>
2024-11-11 00:21:30 -0600

    Se creó el Despachador de Largo Plazo. Se corrigió un error de CreateProcess
    El error de CreteProcess.c recaía sobre el caracter nulo al final de la estructura
    No se consideraba, por lo que causaba error al imprimir.
    
    Se hizo la correción añadiendo una linea, agregando a la localidad
    [5] el caracter nulo.
    
    Nuestro despachador de largo plazo fue agregado.
    Lee y hace sus funciones correspondientes
    
    PENDIENTE
    Agregar semaforos y memoria compartida

--- despachadorLargoPlazo.c
+++ despachadorLargoPlazo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <perror.h>
+#include <unistd.h>
 #include <sys/types.h>
 
 struct Process{
@@ -11,7 +11,7 @@ struct Process{
 	int tWaiting;
 	int priority;
 	char name[5];
-}
+};
 
 /*
 				Despachador de Largo Plazo
@@ -23,11 +23,44 @@ struct Process{
 */
 
 int main(){
-	FILE *fPtr();
+	FILE *fPtr;
+	int n, k;
 	fPtr = fopen("processRequest.dat","rb");
 	if(fPtr==NULL){
 		perror("Error opening file of data\n");
 	} else {
 		//FILE IS OPENED
+
+		//Simulando el estado permanente de lectura, usaremos while 1
+		//Proximamente reemplazado por el semaforo
+		printf("%-6s%-16s%-16s%-6s%-6s\n","Id", "CPU Burst", "Name","CTime","WTime");
+		while(1){
+			//Leemos n cantidad de datos. De 1 a 20
+			n = (rand()%20) + 1;
+			//Esperaremos k segundos. de 1-10
+			k = (rand()%10) + 1;
+			//Lee n cantidad de datos
+			struct Process readed[n];
+			if((fread(readed, sizeof(struct Process), n, fPtr))==n){
+				//Leyó de forma correcta
+				/*
+					Los datos fueron leidos y estan almacenados
+					en un arreglo, se tiene que compartir a
+					memoria compartida.
+					Status: Impresion para prueba
+				*/
+				printf("Lote Leido\n");
+				for(int i=0; i<n; i++){
+					printf("%-6d%-16d%-16s%-6d%-6d\n", readed[i].id,readed[i].cpuBurst, readed[i].name, readed[i].tCompletition, readed[i].tWaiting);
+				}
+			} else {	//Error handling
+				if(feof(fPtr)){
+					perror("Unexpected EOF\n");
+				} else if (ferror(fPtr)){
+					perror("Error reading file\n");
+				}
+			}
+			sleep(k);
+		}
 	}
 }
