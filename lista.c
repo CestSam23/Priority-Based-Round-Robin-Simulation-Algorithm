46db46b47b5b8c4a90dde90e3ffabef7cba9cd9a
Sam <samfertap@gmail.com>
2024-11-11 22:03:41 -0600

    Se añadieron los programas en los que se trabajará
    Se creó lista.h, lista.c. Ambos son para la lista
    En lista.h esta la definición de funciones, mientras en lista.c esta la implementacion
    
    En trial.c podemos crear escenarios para verificar el funcionamiento de la lista enlazada
    Programa es el ejecutable
    
    **NOTA. PARA COMPILACION**
    
    ''gcc trial.c lista.c -o programa''

---
+++ lista.c
@@ -0,0 +1,50 @@
+#include "lista.h"
+#include <stdio.h>
+#include <sys/types.h>
+#define MAX 50
+
+/*
+Implementación de las funciones del archivo cabecera
+Parte del código. Recordemos que usaremos un MAX
+*/
+
+
+int addProcess(struct Process process){
+	return 1;
+}
+
+int addProcesses(struct Process *process){
+	return 1;
+}
+
+struct Process deleteProcess(){
+	
+}
+
+struct Process getprocess(int n){
+
+}
+
+int aumentarEspera(int s){
+
+}
+
+int restarEjecucion(int s){
+
+}
+
+void ordenarPorPrioridad(){
+
+}
+
+int size(){
+
+}
+
+int isEmpty(){
+
+}
+
+const char *toString(){
+
+}
