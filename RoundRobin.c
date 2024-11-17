edf0a1949c9f20567690c7a8db152a3ef9add443
Sam <samfertap@gmail.com>
2024-11-17 14:43:22 -0600

    Modificacion de estructura de codigo y agregamos la funcionalidad
    La modificacion de archivo fue para hacerlo mas ordenado y legible
    Separando por módulos las funciones
    
    Se agregó la lógica detras de las funciones
    
    PENDIENTE
    Agregar funcionalidad con memoria comaprtida y semaforos

--- RoundRobin.c
+++ RoundRobin.c
@@ -3,17 +3,64 @@
 #define MAX_PROCESSES 500
 #define QUANTUM 5
 
-// Variables globales para estadísticas
-int procesos_ejecutados = 0, procesos_aniquilados = 0, tiempo_total_espera = 0;
+/*
+    *************DEFINICION DE FUNCIONES QUE OCUPAREMOS**********
+*/
+
+//FUNCIONES PARA AUMENTAR/DECREMENTAR SEMAFOROS
+void down();
+void up();
+
+//FUNCIONES DE FINALIZACIÓN
+void manejar_sigterm(int sig);
+void freeResources(int semid, int shmid, lista_t *shm_ptr);
+
+//MODULOS DE LOGICA DE DESPACHADOR
+void LargoPlazo(char *name);
+void CortoPlazo();
+void estadisticas();
+/*  
+    ***************FIN DE DEFINICIONES************
+*/
+
 
 // Identificadores de memoria compartida y semáforos
 int shm_id, sem_id;
 lista_t *shared_data;
 
-// Archivo de estadísticas
-FILE *estadisticas_file; // generado
 
-// Funciones de manejo de semáforos
+/*
+    ****************PROGRAMA PRINCIPAL****************
+*/
+
+int main(){
+    char *archivo_entrada= "processRequest.dat";
+   
+    // Inicialización de la memoria compartida y semáforos
+    shm_id = shmget(IPC_PRIVATE, sizeof(lista_t), IPC_CREAT | 0666);
+    shared_data = (lista_t *)shmat(shm_id, NULL, 0);
+
+    sem_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
+    semctl(sem_id, 0, SETVAL, 1);
+
+    // Configuración de la memoria compartida (inicializar puntero)
+    shared_data->actual = 0;
+    shared_data->size = 0;
+
+    signal(SIGTERM, manejar_sigterm);
+    if (fork() == 0) {
+        LargoPlazo(archivo_entrada);
+    } else {
+        CortoPlazo();
+    }
+
+}
+
+
+
+/**
+    *******************MANEJO DE SEMAFOROS********************
+ */
 void down() {
     struct sembuf op = {0, -1, 0};
     semop(sem_id, &op, 1);
@@ -25,17 +72,144 @@ void up() {
 }
 
 
-// Proceso lector recibe como parametro el archivo processRequest.dat(en función)
+
+
+/*
+    ****************DESPACHADOR LARGO PLAZO*************************
+*/
+
 void LargoPlazo(char *name){
-FILE *fPtr = fopen(name, "rb");
+    
+    FILE *fPtr = fopen(name,"rb");
+    int n,k;
+    if(fPtr==NULL){
+        perror("Error openin file of data\n");
+    } else {
+        while(1){
+            n = (rand()%20) + 1;
+            k = (rand()%10) + 1;
+            process_t readed[n];
+            if((fread(readed,sizeof(process_t),n,fPtr))==n){
+                //Proceso Leido y Almacenado en readed
+                //Enviar a memoria compartida readed
+
+            } else {
+                if(feof(fPtr)){
+                    perror("Unexpected EOF\n");
+                } else if(ferror(fPtr)){
+                    perror("Error reading file\n");
+                    break;
+                }
+            }
+            sleep(k);
+        }
+    }
+
+    //CONSIDERAR
+    //Nunca se cerrará fclose, ya que se ejecuta sigterm
+    fclose(fPtr);
+    
 }
 
-// Proceso despachador (en función)
+
+
+
+/*
+    **************DESPACHADOR CORTO PLAZO**************************
+*/
+
+void roundRobin();
+void priority();
+void sendToAnalytics(process_t process);
+lista_t listaDeProcesos;
+
+
 void CortoPlazo(){
-
+    while(1){
+        //Recibir datos constantemente de memoria compartida
+        //Almacenarlos en listaDeProcesos
+    	roundRobin();
+	    ordenarPorPrioridad();
+	    priority();
+	    printf("\tLOTE DESPACHADO\n");
+    }
 }
+
+void roundRobin(){
+    /*
+	Primera y única ronda de Round Robin
+	Recordemos que por documentacion addProcesses deja
+	apuntando actual al primer elemento agregado
+	*/
+	rewindList();
+	int iterations = size();
+	for(int i=0; i<iterations; i++){
+		printf("---------------------------Proceso %s ----------------------\n", actual().name);
+		aumentarEspera(QUANTUM);
+		aumentarTerminacion(QUANTUM);
+		//Se terminó Proceso, enviar a modulo de estadistica
+		if(restarEjecucion(QUANTUM)==-1){
+			printf("El proceso %s ha sido despachado por completo con RoundRobin\n",actual().name);
+			sendToAnalytics(deleteProcess());
+			printf("*******************************************************\n\n");
+			//No es necesario avanzar. La función delete process
+			//Recorre en automático
+		} else {
+			//Avanzamos a la siguiente estructura
+			//En ultimo elemento, regresamos al inicio
+			
+			printf("\t\tDespachando proceso:%s\n", actual().name);
+			next();
+			printf("*******************************************************\n\n");
+		}
+		
+	}
+}
+
+void priority(){
+    /*
+	Para este punto, los procesos ya están en orden
+	Solamente es despacharlos
+	
+	 */
+	rewindList();
+	int iterations = size();
+	for(int i=0;i<iterations;i++){
+		printf("---------------------------Proceso %s ----------------------\n", actual().name);
+		printf("\t\tDespachando proceso:%s\n", actual().name);
+
+		int remain = actual().cpuBurst;
+		
+		aumentarEspera(remain);
+		aumentarTerminacion(remain);
+		restarEjecucion(remain);
+
+		
+		printf("El proceso %s ha sido despachado por completo con prioridades\n",actual().name);
+
+		sendToAnalytics(deleteProcess());
+	
+	}
+}
+
+/*
+    Función que se encarga de enviar proceso por proceso a la memoria compartida para que la funcion de estadistica pueda leer y almacenar
+*/
+void sendToAnalytics(process_t process){
+	
+}
+
+
+/*
+    **************MODULO DE ESTADISTICA*****************
+*/
+
+// Variables para estadistica
+int procesos_ejecutados = 0, procesos_aniquilados = 0, tiempo_total_espera = 0;
 
 // Módulo estadistica
+FILE *estadisticas_file; // generado
+
 void estadisticas(){
     // Cálculo del tiempo promedio de espera
     double tiempo_promedio_espera = procesos_ejecutados > 0 ? 
@@ -60,6 +234,11 @@ void estadisticas(){
 
 
 
+
+
+
+
+//FINALiZACIÓn DE PROGRAMA
 // Función para manejar SIGTERM
 void manejar_sigterm(int sig) {
     printf("\nRecibiendo señal SIGTERM, generando estadísticas...\n");
@@ -73,27 +252,4 @@ void freeResources(int semid, int shmid, lista_t *shm_ptr) {
     shmdt(shm_ptr);              // Desvincula la memoria compartida
     shmctl(shmid, IPC_RMID, 0);  // Elimina la memoria compartida
 	 semctl(semid, 0, IPC_RMID); // elimina los semaforos 
-}
-
-int main(){
-    char *archivo_entrada= "processRequest.dat";
-   
-    // Inicialización de la memoria compartida y semáforos
-    shm_id = shmget(IPC_PRIVATE, sizeof(lista_t), IPC_CREAT | 0666);
-    shared_data = (lista_t *)shmat(shm_id, NULL, 0);
-
-    sem_id = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
-    semctl(sem_id, 0, SETVAL, 1);
-
-    // Configuración de la memoria compartida (inicializar puntero)
-    shared_data->actual = 0;
-    shared_data->size = 0;
-
-    signal(SIGTERM, manejar_sigterm);
-    if (fork() == 0) {
-        LargoPlazo(archivo_entrada);
-    } else {
-        despachador();
-    }
-
 }
